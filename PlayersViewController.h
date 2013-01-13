ceb248e211e55d3ca9465c0dfd1d67394479b45d
Michael Prenez-Isbell <michael.isbell@gmail.com>
2013-01-13 13:53:31 -0500

    page 137 of storyboards project

---
+++ PlayersViewController.h
@@ -0,0 +1,15 @@
+//
+//  PlayersViewController.h
+//  RatingsRW
+//
+//  Created by Michael Isbell on 1/13/13.
+//  Copyright (c) 2013 Michael Prenez-Isbell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayersViewController : UITableViewController
+
+@property (nonatomic, strong) NSMutableArray *players;
+
+@end
