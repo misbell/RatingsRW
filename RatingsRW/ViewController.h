5d405026140b98a01ad83ac5dfb0d13debbef992
Michael Prenez-Isbell <michael.isbell@gmail.com>
2013-01-13 08:33:57 -0500

    first commit

---
+++ RatingsRW/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RatingsRW
+//
+//  Created by Michael Isbell on 1/13/13.
+//  Copyright (c) 2013 Michael Prenez-Isbell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ RatingsRW/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RatingsRW
+//
+//  Created by Michael Isbell on 1/13/13.
+//  Copyright (c) 2013 Michael Prenez-Isbell. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
